52047332cd4530ca803bf6d7925c6d757ab59b98
Tushar <tushar.nyc20@gmail.com>
2025-09-30 12:12:14 +0530

    added middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import { currentUser } from "@clerk/nextjs/server";
+import Link from "next/link";
 
+export default async function Home() {
+  const user = await currentUser();
 
-export default function Home() {
-  return <div>Home Page</div>;
+  if(!user) {
+    return <h1>Please sign in first</h1>
+  }
+
+  return(
+    <div>
+      <h1>Welcome back, {user.firstName} </h1>
+      <Link href={`/grid`}> Check your progress </Link>
+    </div>
+  )
 }
